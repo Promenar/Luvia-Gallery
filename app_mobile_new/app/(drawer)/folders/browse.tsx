dd127125f1d55274be79d9e3c29f7f32711cf950
Promenar <lengzehua@vip.qq.com>
2025-12-18 22:38:16 +0800

    chore: remove deprecated app_mobile_new directory

--- app_mobile_new/app/(drawer)/folders/browse.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import FolderBrowser from '../../../components/FolderBrowser';
-
-export default function FoldersBrowse() {
-    return <FolderBrowser isRoot={false} />;
-}
--- app_mobile_new/app/(drawer)/folders/index.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import FolderBrowser from '../../../components/FolderBrowser';
-
-export default function FoldersIndex() {
-    return <FolderBrowser isRoot={true} initialPath="root" />;
-}
